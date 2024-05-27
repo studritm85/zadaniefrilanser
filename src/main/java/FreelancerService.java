9c85b867ba5183711c8656a5fa55296de6c6038c
Ekaterina <studritm@gmail.com>
2024-05-27 19:50:35 +0400

    commit

---
+++ src/main/java/FreelancerService.java
@@ -0,0 +1,18 @@
+package ru.netology.services;
+public class FreelancerService {
+    public int calcFreelancerMonths(int income, int expenses, int threshold) {
+        int money = 0;
+        int FreelancerMonths = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money < threshold) {
+                money += income; // money = money + income
+                money -= expenses;
+            } else {
+                money -= expenses;
+                money /= 3;
+                FreelancerMonths++;
+            }
+        }
+        return FreelancerMonths;
+    }
+}
---
+++ src/main/java/FreelancerServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.services;
+
+// Импорт класса FreelancerService
+import ru.netology.services.FreelancerService;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FreelancerServiceTest {
+    @Test
+    public void testCalcFreelancerMonthsBelowThreshold() {
+        FreelancerService service = new FreelancerService();
+        int income = 2000;
+        int expenses = 1500;
+        int threshold = 5000;
+        assertEquals(1, service.calcFreelancerMonths(income, expenses, threshold));
+    }
+
+    @Test
+    public void testCalcFreelancerMonthsExactThreshold() {
+        FreelancerService service = new FreelancerService();
+        int income = 4000;
+        int expenses = 2000;
+        int threshold = 2000;
+        assertEquals(6, service.calcFreelancerMonths(income, expenses, threshold));
+    }
+
+    @Test
+    public void testCalcFreelancerMonthsAboveThreshold() {
+        FreelancerService service = new FreelancerService();
+        int income = 4500;
+        int expenses = 2000;
+        int threshold = 3000;
+        assertEquals(5, service.calcFreelancerMonths(income, expenses, threshold));
+    }
+
+    @Test
+    public void testCalcFreelancerMonthsHighThreshold() {
+        FreelancerService service = new FreelancerService();
+        int income = 3000;
+        int expenses = 1000;
+        int threshold = 20000;
+        assertEquals(1, service.calcFreelancerMonths(income, expenses, threshold));
+    }
+}
